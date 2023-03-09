94bc8c19e645f671ad78ce4241d8207ffe506cf0
Ansh <96686986+Ansh-Kushwaha@users.noreply.github.com>
2023-03-09 07:55:24 +0000

    Lab 2-3

--- ADA/programs/BubbleSort.cpp
+++ ADA/programs/BubbleSort.cpp
@@ -2,15 +2,26 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n) {
+    bool flag;
+    int nSwaps = 0;
+    int nPasses = 0;
     for(int i = 0; i < n - 1; i++) {
+        nPasses++;
+        flag = false;
         for(int j = 0; j < n - i - 1; j++) {
             if(arr[j] > arr[j+1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                nSwaps++;
+                flag = true;
             }
         }
+            if(!flag)
+                break;
     }
+    cout << "Total Passes : " << nPasses << endl;
+    cout << "Total swapping : " << nSwaps << endl;
 }
 
 int main() {
---
+++ ADA/programs/SplitString.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#define INT_MAX 2147483647
+using namespace std;
+
+int main() {
+    string line;
+    cout << "Enter String : ";
+    getline(cin, line);
+
+    int s = line.size();
+    cout << "Lengeth of string : " << s << "\n";
+
+    string currWord = "";
+    string smallest = "";
+    int minSize = INT_MAX;
+    string largest = "";
+    int maxSize = -1;
+
+    // O(n)
+    for(int i = 0; i < s; i++) {
+        if(line[i] != ' ') {
+            currWord += line[i];
+        }
+        else {
+            if((int) currWord.size() < minSize) {
+                minSize = currWord.size();
+                smallest = currWord;
+                // cout << "smallest found" << endl;
+            }
+            if((int) currWord.size() > maxSize) {
+                maxSize = currWord.size();
+                largest = currWord;
+                // cout << "largest found" << endl;
+            }
+            currWord = "";
+        }
+    }
+
+    if(currWord != "") {
+        if(currWord.size() < minSize) {
+            minSize = currWord.size();
+            smallest = currWord;
+        }
+        if(currWord.size() > maxSize) {
+            maxSize = currWord.size();
+            largest = currWord;
+        }
+        currWord = "";
+    }
+
+    cout << "Smallest word : " << smallest << "\n";
+    cout << "Largest Word : " << largest << "\n";
+
+    return 0;
+}
