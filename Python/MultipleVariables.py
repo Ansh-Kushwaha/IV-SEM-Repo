c2fdca9749a3e4d3ed13133659247ea0790cb872
Ansh <96686986+Ansh-Kushwaha@users.noreply.github.com>
2023-03-17 10:57:09 +0000

    17/03 Python

--- Python/MultipleVariables.py
+++ Python/MultipleVariables.py
@@ -1,2 +1,4 @@
 a = b = c = d = 1
+x, y, z = "Orange", "Banana", "Cherry"
 print(a, b, c, d)
+print(x, y, z)
---
+++ Python/Operators.py
@@ -0,0 +1,9 @@
+a, b = 5, 10
+c, d = 4, 2
+e, f = 9, 2
+x, y = 50, 25
+
+print(a + b)
+print(c - d)
+print(e * f)
+print(x / y)
