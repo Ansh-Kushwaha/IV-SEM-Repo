7e62d59a9bf232f7cf3f4052bcad410d21d3fab5
Ansh <96686986+Ansh-Kushwaha@users.noreply.github.com>
2023-03-27 10:52:56 +0000

    Update ADA

--- ADA/Lab Programs/GreatestElement.cpp
+++ ADA/Lab Programs/GreatestElement.cpp
@@ -10,13 +10,15 @@ int greatestI(int arr[], int n) {
 }
 
 int greatestR(int arr[], int n) {
-
+    if(n == 1)
+        return arr[0];
+    return max(arr[n-1], greatestR(arr, n - 1));
 }
 
 int main() {
     int arr[] = {43, 12, 34, 3, 5};
     cout << greatestI(arr, 5) << "\n";
-    cout << greatestR(arr, arr[4], 5) << "\n";
+    cout << greatestR(arr, 5) << "\n";
 
     return 0;
 }
