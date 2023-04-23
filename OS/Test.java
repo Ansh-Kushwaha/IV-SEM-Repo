2471984e0ca26d68c6b07ba6b31a41401fe0982a
Ansh <96686986+Ansh-Kushwaha@users.noreply.github.com>
2023-04-23 14:59:48 +0000

    Deleted and changed programs

--- OS/Test.java
+++
@@ -1,8 +0,0 @@
-class Test {
-    public static void main(String[] args) {
-        FCFS sc = new FCFS();
-        sc.getSchedule();
-
-        System.out.println("Average Waiting time : " + sc.getAvgWaitingTime());
-    }
-}
