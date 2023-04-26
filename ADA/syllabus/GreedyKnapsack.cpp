2394ee9b5fc14e0d2403f855cda157c2083f856e
Ansh <96686986+Ansh-Kushwaha@users.noreply.github.com>
2023-04-26 15:43:28 +0000

    Fix GreedyKnapsack

--- ADA/syllabus/GreedyKnapsack.cpp
+++ ADA/syllabus/GreedyKnapsack.cpp
@@ -1,75 +1,81 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct Item {
+    int id;
     int profit;
     int weight;
 
-    Item(int p, int w) {
+    double proPWt;
+
+    Item(int i, int p, int w) {
+        id = i;
         profit = p;
         weight = w;
+        proPWt = (double) p / w;
     }
 };
 
 bool compareItems(Item i1, Item i2) {
-    double p1 = (double) i1.profit / i1.weight;
-    double p2 = (double) i2.profit / i2.weight;
-    return p1 > p2;
+    return i1.proPWt > i2.proPWt;
 }
 
 void greedyKnapsack(Item items[], int m, int n) {
-    int sortedIdx[n];
-    bool s[n];
-    for(int i = 0; i < n; i++)
-        s[i] = false;
+    int orIdx[n];
     for(int i = 0; i < n; i++) {
         int maxIdx = i;
         for(int j = i + 1; j < n; j++) {
             if(compareItems(items[j], items[i]))
                 maxIdx = j;
         }
-        if(!s[maxIdx]) {
-            sortedIdx[i] = maxIdx;
-            s[maxIdx] = true;
+        if(maxIdx != i) {
+            Item temp = items[i];
+            items[i] = items[maxIdx];
+            items[maxIdx] = temp;
         }
-
     }
 
-    for(int i = 0; i < n; i++)
-        cout << sortedIdx[i] << " ";
-    cout << endl;
+    for(int i = 0; i < n; i++) 
+        orIdx[i] = (items[i].id - 1);
 
     double result[n];
+
     for(int i = 0; i < n; i++)
         result[i] = 0.0;
 
     int u = m;
+    double maxProfit = 0;
     int i;
+    
     for(i = 0; i < n; i++) {
-        if(items[sortedIdx[i]].weight > u)
+        if(items[i].weight > u)
             break;
         else {
-            u = u - items[sortedIdx[i]].weight;
-            result[sortedIdx[i]] = 1;
+            u = u - items[i].weight;
+            maxProfit += items[i].profit;
+            result[orIdx[i]] = 1;
         }
     }
-    if(i != n)
-        result[sortedIdx[i]] = (double) u / items[i].weight;
+    if(i != n) {
+        result[orIdx[i]] = (double) u / items[i].weight;
+        maxProfit += result[orIdx[i]] * items[i].profit;
+    }
     
     cout << "[";
     for(int i = 0; i < n; i++) {
-        cout << "x" << (i + 1) << " : " << result[sortedIdx[i]];
+        cout << "x" << (i + 1) << " : " << result[i];
         if(i != n - 1)
             cout << ", ";
     }
     cout << "]" << endl;
+    cout << "Maximum Profit : " << maxProfit << endl;
 }
 
 int main() {
     int m = 20;
-    Item items[] = { {10, 15},
-                     {20, 15},
-                     {30, 10} };
+    Item items[] = { {1, 10, 15},
+                     {2, 20, 15},
+                     {3, 30, 10} };
     int n = sizeof(items) / sizeof(items[0]);
     greedyKnapsack(items, m, n);
 
