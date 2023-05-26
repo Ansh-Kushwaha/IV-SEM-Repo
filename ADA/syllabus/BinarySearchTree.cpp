0dd17ddc50563668bf55239ccbc0c74e6bef9468
Ansh <96686986+Ansh-Kushwaha@users.noreply.github.com>
2023-05-26 07:38:08 +0000

    Completed BST class

--- ADA/syllabus/BinarySearchTree.cpp
+++ ADA/syllabus/BinarySearchTree.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <cmath>
 using namespace std;
 
 struct node {
@@ -17,13 +19,14 @@ class BST {
     private:
     node *root;
 
-    node* search(int x, node* root) {
-        if(root->data == x || root == NULL)
-            return root;
-        else if(x < root->data)
-            return search(x, root->left);
-        else
-            return search(x, root->right);
+    node* search(int x, node* node) {
+        if(node == NULL)
+            return NULL;
+        else if(x < node->data)
+            return search(x, node->left);
+        else if(x > node->data)
+            return search(x, node->right);
+        else return node;
     }
 
     node* findInorderSuccessor(node* root) {
@@ -34,12 +37,34 @@ class BST {
         return root;
     }
 
+    int heightOfTree(node* root) {
+        if (root == NULL)
+            return 0;
+        return 1 + max(heightOfTree(root->left), heightOfTree(root->right));
+    }
+
+    void preorder(node* root) {
+        if(root == NULL)
+            return;
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+
     void inorder(node* root) {
         if(root == NULL)
             return;
         inorder(root->left);
         cout << root->data << " ";
         inorder(root->right);
+    }
+
+    void postorder(node* root) {
+        if(root == NULL)
+            return;
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
     }
 
     node* insertNode(node* curr, int x) {
@@ -86,6 +111,48 @@ class BST {
         return root;
     }
 
+    void printSpace(double n, node* removed) {
+        for(; n > 0; n--)
+            cout << "\t";
+        if(removed == NULL)
+            cout << " ";
+        else
+            cout << removed->data;
+    }   
+
+    void printBinaryTree(node* root) {
+        queue<node*> treeLevel, temp;
+        treeLevel.push(root);
+        int counter = 0;
+        int height = heightOfTree(root) - 1;
+        double numberOfElements = pow(2, (height + 1)) - 1;
+        while(counter <= height) {
+            node* removed = treeLevel.front();
+            treeLevel.pop();
+            if(temp.empty()) {
+                printSpace(numberOfElements / pow(2, counter + 1), removed);
+            }
+            else {
+                printSpace(numberOfElements / pow(2, counter), removed);
+            }
+            if (removed == NULL) {
+                temp.push(NULL);
+                temp.push(NULL);
+            }
+            else {
+                temp.push(removed->left);
+                temp.push(removed->right);
+            }
+            if(treeLevel.empty()) {
+                cout << endl << endl;
+                treeLevel = temp;
+                while (!temp.empty())
+                    temp.pop();
+                counter++;
+            }
+        }
+    }
+
     public:
     BST() {
         root = NULL;
@@ -97,6 +164,8 @@ class BST {
 
     bool search(int key) {
         node* temp = search(key, root);
+        if(temp == NULL)
+            return false;
         return true;
     }
 
@@ -104,8 +173,46 @@ class BST {
         deleteNode(key, root);
     }    
 
+    int getHeight() {
+        return heightOfTree(root);
+    }
+
+    void preorder() {
+        cout << "Preorder: ";
+        preorder(root);
+        cout << "\n";
+    }
+
     void inorder() {
+        cout << "Inorder: ";
         inorder(root);
+        cout << "\n";
+    }
+
+    void postorder() {
+        cout << "Postorder: ";
+        postorder(root);
+        cout << "\n";
+    }
+
+    void levelorder() {
+        cout << "Levelorder: ";
+        queue<node*> q;
+        q.push(root);
+        while(!q.empty()) {
+            node* curr = q.front();
+            q.pop();
+            cout << curr->data << " ";
+            if(curr->left != NULL)
+                q.push(curr->left);
+            if(curr->right != NULL)
+                q.push(curr->right);
+        }
+        cout << "\n";
+    }
+
+    void showTree() {
+        printBinaryTree(root);
     }
 };
 
@@ -120,13 +227,24 @@ int main() {
     tree.insert(12);
     tree.insert(20);
     tree.insert(50);
+    tree.insert(9);
 
+    tree.preorder();
     tree.inorder();
+    tree.postorder();
     cout << "\n";
+    tree.showTree();
     tree.deleteNode(45);
+
+    tree.preorder();
     tree.inorder();
-    cout << "\n";
-    tree.search(9);
+    tree.postorder();
+    if(tree.search(8))
+        cout << 8 << " is present in the tree\n";
+    else   
+        cout << 8 << " is not present in the tree\n";
+    tree.showTree();
+    tree.levelorder();
 
     return 0;
 }
